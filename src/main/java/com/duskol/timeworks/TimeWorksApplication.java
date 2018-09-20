c727b636e89463d4c8effa8996c78d6cf578ebb2
duskol <dulleo@gmail.com>
2018-09-20 13:21:16 +0200

    Initial commit

---
+++ src/main/java/com/duskol/timeworks/TimeWorksApplication.java
@@ -0,0 +1,12 @@
+package com.duskol.timeworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeWorksApplication.class, args);
+	}
+}
