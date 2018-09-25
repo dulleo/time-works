bdebf0a4e450c0b53e97e7569c191a2ec73b4bc9
duskol <dulleo@gmail.com>
2018-09-25 13:30:08 +0200

    Initial commit

---
+++ src/main/java/com/duskol/timeworks/exception/DeveloperAlreadyAddedToProjectException.java
@@ -0,0 +1,8 @@
+package com.duskol.timeworks.exception;
+
+public class DeveloperAlreadyAddedToProjectException extends Exception {
+
+    public DeveloperAlreadyAddedToProjectException(String message)  {
+        super(message);
+    }
+}
