b516d09aa3bcb062851878299527bf7722af06c4
duskol <dulleo@gmail.com>
2018-09-25 13:28:50 +0200

    Initial commit

---
+++ src/main/java/com/duskol/timeworks/dto/ProjectDto.java
@@ -0,0 +1,14 @@
+package com.duskol.timeworks.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectDto {
+
+    private String id;
+    private String title;
+}
