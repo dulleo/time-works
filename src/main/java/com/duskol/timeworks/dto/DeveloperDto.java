610552717be1e904b4fc8d4dbb126371e9c1e234
duskol <dulleo@gmail.com>
2018-09-25 13:28:22 +0200

    Initial commit

---
+++ src/main/java/com/duskol/timeworks/dto/DeveloperDto.java
@@ -0,0 +1,14 @@
+package com.duskol.timeworks.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeveloperDto {
+
+    private String username;
+    private String totalHoursWorked;
+}
